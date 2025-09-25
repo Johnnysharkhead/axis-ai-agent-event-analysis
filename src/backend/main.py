edc6dd2af076b3e6de7897406116b38cf163c04f
augek130 <augek130@tlvm-4-1-1.ad.liu.se>
2025-09-25 16:20:06 +0200

    More backend initial setup

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,15 @@
  #!/usr/bin/env python3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(port=5000) # På MacOS, byt till 5001 eller dylikt
