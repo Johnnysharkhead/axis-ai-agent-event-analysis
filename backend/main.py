8520a37d359892286a81e35afc5cce2ba32cb8e9
August <augek130@student.liu.se>
2025-10-01 16:19:03 +0200

    La till test för att få videofeed på en backend route

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 import os
+from livestream import VideoCamera
 
 app = Flask(__name__)
 
@@ -18,10 +19,25 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+camera = VideoCamera()
+
+def generate_frames():
+    """Generate frames for the video stream"""
+    while True:
+        frame = camera.get_frame()
+        if frame is not None:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 # Example route
 @app.route("/")
 def index():
     return "Hello from Flask with an auto-created DB!"
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     with app.app_context():
