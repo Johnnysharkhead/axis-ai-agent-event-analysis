74bfae015fb98cf460e25a9bd8085ff51d957044
Jonatan Billger <jonathan.billger@liuformulastudent.se>
2025-10-02 14:12:17 +0200

    Added footer to bottom of homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import React from "react";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
