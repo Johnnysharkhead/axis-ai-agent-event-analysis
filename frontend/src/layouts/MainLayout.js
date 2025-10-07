12a608b0d6a4743025069cfc2e607cf81725babe
Olof HojebergLindstrand <oloho489@student.liu.se>
2025-10-07 11:08:12 +0200

    Sidebar has been fixed but no routes have been added

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -6,15 +6,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
 
 function MainLayout({ children }) {
   return (
     <div>
       <Navbar />
-      <div style={{ padding: "1rem" }}>{children}</div>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ flex: 1, padding: "1rem" }}>{children}</div>
+      </div>
       <Footer />
     </div>
   );
 }
+
 
 export default MainLayout;
