76e3b3acf355029e742d0e1a5f384d6b50eb1f34
Jonatan Billger <jonathan.billger@liuformulastudent.se>
2025-10-16 18:08:55 +0200

    Recording added to database

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, Response, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 # import requests
 import os
@@ -78,6 +79,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     User, InviteKey, Room, Camera, Recording, Metadata = models.init_models(db) 
